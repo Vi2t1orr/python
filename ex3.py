a7ce32e0c1a683c817ee2fedf7262fe1d8c5c5b1
Vi2t1orr <141885532+Vi2t1orr@users.noreply.github.com>
2024-06-07 11:37:22 -0300

    Create ex3.py

---
+++ ex3.py
@@ -0,0 +1,10 @@
+x = 11
+
+for n in range(1, x):
+  print(n)
+
+menor = min(range(1, x))
+maior = max(range(1, x))
+
+print(f"O menor numero entre 1 e 10 é: {menor}")
+print(f"O maior numero entre 1 e 10 é: {maior}")
